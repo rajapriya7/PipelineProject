8c38ea9f4896f98ce91d115a208cca60c26b8a2a
rajapriya7 <rajapriyakalimuthu@gmail.com>
2022-08-23 19:18:06 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PipelineProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
